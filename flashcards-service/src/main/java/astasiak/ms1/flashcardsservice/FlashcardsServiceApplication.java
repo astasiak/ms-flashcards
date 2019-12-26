416bc99bc6982f19185b0e4981dce4402663f876
Andrzej Stasiak <astasiak@example.com>
2019-12-26 23:51:41 +0100

    Added discovery server

--- flashcards-service/src/main/java/astasiak/ms1/flashcardsservice/FlashcardsServiceApplication.java
+++ flashcards-service/src/main/java/astasiak/ms1/flashcardsservice/FlashcardsServiceApplication.java
@@ -2,9 +2,11 @@ package astasiak.ms1.flashcardsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FlashcardsServiceApplication {
 
 	@Bean
