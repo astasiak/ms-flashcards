f4d14a7fa4f83996adfd0be1e1478e29e00da9df
Andrzej Stasiak <astasiak@example.com>
2019-12-26 02:35:20 +0100

    Added microservice for flashcards

---
+++ flashcards-service/src/main/java/astasiak/ms1/flashcardsservice/FlashcardsServiceApplication.java
@@ -0,0 +1,13 @@
+package astasiak.ms1.flashcardsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsServiceApplication.class, args);
+	}
+
+}
---
+++ flashcards-service/src/test/java/astasiak/ms1/flashcardsservice/FlashcardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package astasiak.ms1.flashcardsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
